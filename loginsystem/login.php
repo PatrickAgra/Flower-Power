12fc3b0623231b04ee1c2ad476f453e85abd8cc1
PatrickAgra <98827289+PatrickAgra@users.noreply.github.com>
2022-12-01 02:44:02 +0100

    Add files via upload

--- loginsystem/login.php
+++ loginsystem/login.php
@@ -8,30 +8,30 @@ $pass = ($_POST['password']);
 
 
 if (empty($uname)) {
-    header("Location: loginpage.php?error=User Name is required");
+    header("Location: loginpage.php?error=Gebruikersnaam is vereisd");
     exit();
 } else if (empty($pass)) {
-    header("Location: loginpage.php?error=Password is required");
+    header("Location: loginpage.php?error=Wachtwoord is vereisd");
     exit();
 } else {
-    $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+    $sql = "SELECT * FROM klanten WHERE gebruikersnaam='$uname' AND wachtwoord='$pass'";
 
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) === 1) {
         $row = mysqli_fetch_assoc($result);
-        if ($row['user_name'] === $uname && $row['password'] === $pass) {
-            $_SESSION['user_name'] = $row['user_name'];
-            $_SESSION['name'] = $row['name'];
-            $_SESSION['id'] = $row['id'];
+        if ($row['gebruikersnaam'] === $uname && $row['wachtwoord'] === $pass) {
+            $_SESSION['gebruikersnaam'] = $row['gebruikersnaam'];
+            $_SESSION['voornaam'] = $row['voornaam'];
+            $_SESSION['klantcode'] = $row['klantcode'];
             header("Location: home.php");
             exit();
         } else {
-            header("Location: loginpage.php?error=Incorrect User name or Password");
+            header("Location: loginpage.php?error=Onjuist gebruikersnaam of wachtwoord");
             exit();
         }
     } else {
-        header("Location: loginpage.php?error=Incorrect User name or Password");
+        header("Location: loginpage.php?error=Onjuist gebruikersnaam of wachtwoord");
         exit();
     }
 }
--- loginsystem/signup-check.php
+++ loginsystem/signup-check.php
@@ -3,46 +3,76 @@
 session_start();
 include "db_connect.php";
 
+//extra data dit keer, test
+
 $name = ($_POST['name']);
+$prefixes = ($_POST['prefixes']);
+$surname = ($_POST['surname']);
+$email = ($_POST['email']);
+$adress = ($_POST['adress']);
+$postalcode = ($_POST['postalcode']);
+$place = ($_POST['place']);
+$birthdate = ($_POST['birthdate']);
 $uname = ($_POST['uname']);
 $pass = ($_POST['password']);
 $re_pass = ($_POST['re_password']);
 
+//
 
-$user_data = 'uname=' . $uname . '& name=' . $name;
 
-if (empty($uname)) {
-    header("Location: signup.php?error=Username is required&$user_data");
+$user_data = 'name=' . $name . '& prefixes=' . $prefixes . '& surname=' . $surname . '& email=' . $email . '& adres=' . $adress . '& postalcode=' . $postalcode . '& place=' . $place . '& uname=' . $uname;
+
+
+if (empty($name)) {
+    header("Location: signup.php?error=Voornaam is vereisd&$user_data");
+    exit(); 
+} else if (empty($surname)) {
+    header("Location: signup.php?error=Achternaam is vereisd&$user_data");
+    exit(); 
+} else if (empty($email)) {
+    header("Location: signup.php?error=E-mail is vereisd&$user_data");
+    exit();
+} else if (empty($adress)) {
+    header("Location: signup.php?error=Adres is vereisd&$user_data");
+    exit();
+} else if (empty($postalcode)) {
+    header("Location: signup.php?error=Postcode is vereisd&$user_data");
+    exit();
+} else if (empty($place)) {
+    header("Location: signup.php?error=Woonplaats is vereisd&$user_data");
+    exit();
+} else if (empty($birthdate)) {
+    header("Location: signup.php?error=Geboortedatum is vereisd&$user_data");
+    exit();
+} else if (empty($uname)) {
+    header("Location: signup.php?error=Gebruikersnaam is vereisd&$user_data");
     exit();
 } else if (empty($pass)) {
-    header("Location: signup.php?error=Password is required&$user_data");
+    header("Location: signup.php?error=Wachtwoord is vereisd&$user_data");
     exit();
 } else if (empty($re_pass)) {
-    header("Location: signup.php?error=Confirmation password is required&$user_data");
-    exit();
-} else if (empty($name)) {
-    header("Location: signup.php?error=User Name is required&$user_data");
+    header("Location: signup.php?error=Wachtwoord controleren is vereisd&$user_data");
     exit();
 } else if ($pass !== $re_pass) {
-        header("Location: signup.php?error=The confirmation password does not match&$user_data");
+        header("Location: signup.php?error=Het controle wachtwoord komt niet overeen&$user_data");
         exit();
 } else {
-    $sql = "SELECT * FROM users WHERE user_name='$uname' ";
+    $sql = "SELECT * FROM klanten WHERE gebruikersnaam='$uname' ";
 
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
-        header("Location: signup.php?error=This username is taken please try another&$user_data");
+        header("Location: signup.php?error=Deze gebruikersnaam is al in gebruik&$user_data");
         exit();
     } else {
-        $sql2 = "INSERT INTO users(user_name, password, name) 
-                 VALUES ('$uname', '$pass', '$name')";
+        $sql2 = "INSERT INTO klanten(voornaam, tussenvoegsels, achternaam, email, adres, postcode, woonplaats, geboortedatum, gebruikersnaam, wachtwoord) 
+                 VALUES ('$name', '$prefixes', '$surname', '$email', '$adress', '$postalcode', '$place', '$birthdate', '$uname', '$pass')";
         $result2 = mysqli_query($conn, $sql2);
         if ($result2) {
-            header("Location: signup.php?success=Your account has been created successfully");
+            header("Location: signup.php?success=Je account is succesvol aangemaakt");
             exit();
         } else {
-            header("Location: signup.php?error=unknown error occurred&$user_data");
+            header("Location: signup.php?error=Onverwachtse fout. Probreer het later opnieuw&$user_data");
             exit();
         }
     }
